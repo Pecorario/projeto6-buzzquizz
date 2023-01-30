f5ea5ffa83227a616673ac134572bc9df383129c
Tamara Ferreira <tamarablacksmith@hotmail.com>
2023-01-30 12:29:43 -0300

    fenalização da tela 2.1

--- quizz.js
+++ quizz.js
@@ -16,6 +16,9 @@ function carregarRespostas(data) {
       ${montaPerguntas(data.questions)}
     </div>
     `;
+    document.querySelectorAll('.respostas').forEach((resposta) => {
+      resposta.addEventListener('click', selecionaResposta);
+    });
 }
 
 let perguntas = [];
@@ -25,21 +28,22 @@ function carregarPerguntas(id) {
   axios
     .get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
     .then(resposta => {
-      console.log(resposta);
       carregarRespostas(resposta.data);
+      window.scrollTo(0, 0);
     });
 }
+
 
 function montaPerguntas(perguntas) {
   let perguntashtml = "";
 
-  perguntas.forEach(pergunta => {
+  perguntas.forEach((pergunta, index) => {
     console.log(pergunta);
     perguntashtml += 
-    `<div class="perguntas">
+    `<div class="perguntas" id="${index}">
       <p style="background:${pergunta.color}">${pergunta.title}</p>
       <div class="respostas-div">
-        ${montaRespostas(pergunta.answers)}
+        ${montaRespostas(pergunta.answers, index)}
       </div>
     </div>`
 
@@ -48,12 +52,13 @@ function montaPerguntas(perguntas) {
   return perguntashtml
 }
 
-function montaRespostas(respostas) {
-  let respostashtml = "";
 
-  respostas.forEach(resposta => {
+function montaRespostas(respostas, indicePergunta) {
+  let respostashtml = "";
+  const respostasEmbaralhadas = embaralhaRespostas(respostas);
+  respostasEmbaralhadas.forEach(resposta => {
     respostashtml += 
-    `<div class="respostas" data-certo="${resposta.isCorrectAnswer}" onclick="selecionaResposta(this)">
+    `<div class="respostas" data-certo="${resposta.isCorrectAnswer}" data-pergunta="${indicePergunta}">
       <img src="${resposta.image}" class="respostas-img">
       <p class="texto-resposta">${resposta.text}</p>
     </div>`
@@ -62,6 +67,39 @@ function montaRespostas(respostas) {
   return respostashtml
 }
 
-function selecionaResposta(selecionado) {
+function selecionaResposta() {
+  console.log(this);
+  this.dataset.clicado = true;
+  carregaCoresRespostas(this.dataset.pergunta, this);
+  setTimeout(() => {
+    document.getElementById(`${parseInt(this.dataset.pergunta) + 1}`).scrollIntoView(false);
+  }, 2000);
+}
 
+function embaralhaRespostas(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function carregaCoresRespostas(indicePergunta, respostaClicada){
+  const respostasCores = document.querySelectorAll(`[data-pergunta="${indicePergunta}"]`);
+  console.log(respostaClicada);
+  
+  respostasCores.forEach((resposta) => {
+    console.log(resposta)
+    if(resposta.dataset.certo === "true"){
+      resposta.style.color= "#009C22";
+    } else {
+      resposta.style.color=  "#FF4B4B"
+    }
+    if(!resposta.dataset.clicado){
+      resposta.style.opacity= "0.3";
+    } 
+    resposta.removeEventListener('click', selecionaResposta);
+  });
 }
